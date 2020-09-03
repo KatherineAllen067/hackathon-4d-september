aa235a5e0d919fa3df79f783e961693c8df86016
katherineallen067 <katherineallen067@gmail.com>
2020-09-03 15:13:44 -0400

    setup folder and some partials mixins for hackathon

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+// import React, { Component } from "react";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <h1>4D</h1>
+    </>
+  );
+}
+
+export default App;
