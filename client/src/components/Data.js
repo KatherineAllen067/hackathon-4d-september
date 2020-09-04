b61bd7d295f6cd64a8b1cc3a96eb5ffa1861ea54
Mesbah <64983129+MesbahO@users.noreply.github.com>
2020-09-04 13:58:06 -0700

    last

--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -1,14 +1,11 @@
 import React from "react";
+import "../styles/Data.scss";
 
 function Data() {
   return (
     <>
-      <h1>Map of Local Resturants</h1>  
-      <div>
-        <span>
-          Local means Mom and Pop. 
-        </span>
-      </div>    
+      <h1 className="data-header">Map of Local Resturants</h1>
+      <div className="data-text">Local means Mom and Pop</div>
     </>
   );
 }
