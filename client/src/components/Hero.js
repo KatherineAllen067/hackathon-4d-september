552f2de1cafa9a826b2872131a81a9949aebc694
Mesbah <64983129+MesbahO@users.noreply.github.com>
2020-09-04 12:51:45 -0700

    updated the hero background and added the order botton

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "../styles/Hero.scss";
+import Header from "./Header";
 
 export default function Hero() {
   return (
     <div className="hero-wrap">
+      <Header />
       <div className="hero-wrap__header">Local Reimagined</div>
       <div className="hero-wrap__body">Skip the Dishes, not your local</div>
+      <button className="hero-wrap__order">ORDER NOW</button>
     </div>
   );
 }
-
-export default Hero;
