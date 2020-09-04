974b47a1e1d8f02717c28a4d0f99d19e552ed9aa
katherineallen067 <katherineallen067@gmail.com>
2020-09-04 12:04:47 -0400

    component split and in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.scss';
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
+import Intro from './components/Intro.js';
+import Program from './components/Program.js';
+import Data from './components/Data.js';
 // import React, { Component } from "react";
 // import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <h1>4D</h1>
+    {/* katherine */}
+    <Header />
+    {/* Mesbah */}
+    <Hero />
+    {/* Mesbah */}
+    <Intro />
+    {/* katherine */}
+    <Program />
+    {/* TBD */}
+    <Data />
+
     </>
   );
 }
