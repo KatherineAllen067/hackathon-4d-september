8af0b521379299b6cc9c1abe4a0c71b646f3e660
katherineallen067 <katherineallen067@gmail.com>
2020-09-04 13:24:36 -0400

    conflict fixed

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,14 @@ import Hero from './components/Hero.js';
 import Intro from './components/Intro.js';
 import Program from './components/Program.js';
 import Data from './components/Data.js';
-// import React, { Component } from "react";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    {/* katherine */}
     <Header />
-    {/* Mesbah */}
     <Hero />
-    {/* Mesbah */}
     <Intro />
-    {/* katherine */}
     <Program />
-    {/* TBD */}
     <Data />
     </>
   );
---
+++ client/src/components/Intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Intro(){
+    return(
+        <>
+        <h1>Intro</h1>
+        </>
+    )
+}
+
+export default Intro;
