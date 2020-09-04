20377de0c9fb1e3a4f17b1878e19adfb4a28fa15
katherineallen067 <katherineallen067@gmail.com>
2020-09-04 16:22:52 -0400

    howit works done

--- client/src/components/Program.js
+++ client/src/components/Program.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../styles/Program.scss';
+import Plates from '../assets/icons/platesgif.gif';
+import Enjoy from '../assets/icons/enjoy.png';
+import Redeem from '../assets/icons/redeem.png';
 
 
 function Program(){
@@ -9,16 +12,19 @@ function Program(){
             <h1 className="program-title">How it Works</h1>
             <div className="program-box">
                 <div className="program-card">
+                    <img src={Plates} alt="plate" className="icon2"/>
                     <h2>Earn</h2>
-                    <span></span>
+                    <span>Every-time you order from local restaurants you earn dishes.</span>
                 </div>
                 <div className="program-card">
+                    <img src={Enjoy} alt="enjoy" className="icon2"/>
                     <h2>Redeem</h2>
-                    <span></span>
+                    <span>Get your favourite things for free. Cash in your dishes for delicious rewards and merchandise.</span>
                 </div>
-                <div className="program-card">
+                <div className="program-card" >
+                    <img src={Redeem} alt="redeem" className="icon2"/>
                     <h2>Enjoy</h2>
-                    <span></span>
+                    <span>Sit back and enjoy your goodies. Support local businesses has never been so rewarding.</span>
                 </div>
             </div>
         </div>
